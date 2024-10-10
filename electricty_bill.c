e3f0705007b9ce671ac4664f0b95ac9490cdcc21
Gidy97 <gedionc741@gmail.com>
2024-10-11 00:10:22 +0300

    Add files via upload
    Calculating electricity bill

---
+++ electricty_bill.c
@@ -0,0 +1,54 @@
+//program to calculate electric bill
+/*
+program to calculate electric bill
+Author:Gedion Kimutai Cheruiyot
+Reg No:CT101/G/24133/24
+Date:10/10/2024
+*/
+//preprocessor directive-printf(),scanf() 
+
+#include <stdio.h>
+
+int main() {
+    int customerID;
+    char customerName[100];
+    float unitsConsumed;
+	float totalcharges;
+	
+    printf("Enter Customer ID: ");
+    scanf("%d", &customerID);
+	printf("Customer ID: %d\n", customerID);
+
+    printf("Enter Customer Name: ");
+    scanf(" %[^\n]", customerName); 
+    printf("Customer Name: %s\n", customerName);
+    
+    printf("Enter Units Consumed: ");
+    scanf("%f", &unitsConsumed);
+    printf("Units Consumed: %.2f\n", unitsConsumed);
+    
+    if(unitsConsumed<=65.5){
+    	totalcharges=100
+    	;printf("The totalcharges is %.1f \n",totalcharges);}
+    	
+    else if(unitsConsumed <=199){
+		  totalcharges=unitsConsumed*1.20;
+		  printf("The totalcharges is %.2f \n",totalcharges);
+	  }
+	
+	else if(unitsConsumed<=399){
+		  totalcharges=unitsConsumed*1.50;
+		  printf("The totalcharges is %.2f \n",totalcharges);
+	  }
+	else if(unitsConsumed<=599){
+		  totalcharges=unitsConsumed*1.80;
+		  printf("The totalcharges is %.2f \n",totalcharges);
+	  }
+	else{
+			(unitsConsumed>=600);
+		  totalcharges=unitsConsumed*2.00;
+		  printf("The totalcharges is %.2f \n",totalcharges);
+	  }
+	  
+    return 0;
+}
