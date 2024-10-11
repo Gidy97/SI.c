cd83ff4fa31b8e5573e31e22d33a39046dc3f3e3
Gidy97 <gedionc741@gmail.com>
2024-10-11 14:44:39 +0300

    Add files via upload
    while loop

---
+++ while loop.c
@@ -0,0 +1,19 @@
+// while loop for no 1-50, their squares and sums
+#include <stdio.h>
+
+int main() {
+    int i = 1, sum = 0, sumOfSquares = 0;
+
+    while (i <= 50) {
+        sum += i; 
+        sumOfSquares += i * i;  
+        printf(" %d\t%d\n", i, i * i); 
+        i++;
+    }
+
+	
+    printf("\nSum of numbers from 1 to 50: %d\n", sum);
+    printf("Sum of squares of numbers from 1 to 50: %d\n", sumOfSquares);
+
+    return 0;
+}
