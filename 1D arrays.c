025dc79f3fa9b71439f1a615b4fa3ae7f015a964
Gidy97 <gedionc741@gmail.com>
2024-10-15 08:45:13 +0300

    Add files via upload
    Arrays in c -1D arrays

---
+++ 1D arrays.c
@@ -0,0 +1,10 @@
+//Arrays in c - 1D arrays
+#include<stdio.h>
+int main (){
+	int i;
+	int marks[6]={40,50,60,80,30,92};
+	for(i=0;i<6;i++){
+      printf("marks[%d]= %d \n",i,marks[i]);
+	}
+	return 0;
+}
